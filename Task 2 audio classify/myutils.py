bfb63db29a1fe268019c25a6f54bc9f49b29af42
ammarmuflih <ammarmuflih95@gmail.com>
2026-03-08 17:06:24 +0700

    update task 2

--- Task 2 audio classify/myutils.py
+++ Task 2 audio classify/myutils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import librosa
+import random
 
 def feature_extraction(audio_signal):
     y, sr = librosa.load(audio_signal)
@@ -35,5 +36,34 @@ def feature_extraction(audio_signal):
         'mean_mfcc': np.mean(mfcc_feat, axis=1),
         'std_mfcc': np.std(mfcc_feat, axis=1)
     }
-   
     return feature
+
+class Augmentation():
+    def __init__(self, noise_factor, time_shift_max, pitch_step, t_stretch_rate, volume_scale_factor):
+        self.noise_factor = noise_factor
+        self.time_shift_max = time_shift_max
+        self.pitch_step = pitch_step
+        self.t_stretch_rate = t_stretch_rate
+        self.volume_scale_factor = volume_scale_factor
+    
+    def noise_inject(self, audio_signal):
+        noise = np.random.randn(len(audio_signal))
+        augmented = audio_signal + self.noise_factor * noise
+        return augmented
+    
+    def time_shift(self, audio_signal):
+        shift = int(np.random.uniform(-self.time_shift_max, self.time_shift_max) * len(audio_signal))
+        augmented = np.roll(audio_signal, shift)
+        return augmented
+    
+    def pitch_shift(self, audio_signal, sr):
+        augmented = librosa.effects.pitch_shift(y=audio_signal, sr=sr, n_steps=random.choice(self.pitch_step))
+        return augmented
+    
+    def time_stretch(self, audio_signal):
+        augmented = librosa.effects.time_stretch(y=audio_signal, rate=self.t_stretch_rate)
+        return augmented
+    
+    def volume_scale(self, audio_signal):
+        augmented = audio_signal * random.choice(self.volume_scale_factor)
+        return augmented
